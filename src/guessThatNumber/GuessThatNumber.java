8ab899257fd5a36518cc6393835b65a3dbbe2dff
DanielleHall <halldan4@yahoo.com>
2016-05-06 08:34:47 -0400

    first commit

---
+++ src/guessThatNumber/GuessThatNumber.java
@@ -0,0 +1,16 @@
+package guessThatNumber;
+import java.util.*;
+public class GuessThatNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner (System.in);
+		Random rando = new Random ();
+		int randomNum = rando.nextInt(100);
+		int tryCount=0;
+		
+		System.out.println("Welcome to the NUMBER GUESSING GAME!");
+
+	}
+
+}
